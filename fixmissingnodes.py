723561827998c1d1ad6d0c09b5a45c8d216b206b
Tim <tim2009@sheerman-chase.org.uk>
2014-10-22 21:03:33 +0100

    Add command line options to parse various targets

--- fixmissingnodes.py
+++ fixmissingnodes.py
@@ -1,4 +1,4 @@
-import osm, bz2, urllib2, urlutil, osmmod, os, osmmod, sys
+import osm, bz2, urllib2, urlutil, osmmod, os, osmmod, sys, argparse
 import xml.etree.ElementTree as ET
 
 def WayIsComplete(way, nodes):
@@ -103,41 +103,71 @@ def CheckFile(fiHandle, username, password):
 
 def WalkFiles(di, username, password):
 
-	#username = "TimSCStreetViewImport" #raw_input("Username:")
-	#password = raw_input("Password:")
-
 	for fi in os.listdir(di):
 		if os.path.isdir(di+"/"+fi):
 			WalkFiles(di+"/"+fi, username, password)
 		if os.path.isfile(di+"/"+fi):
-			print di+"/"+fi
-
-			CheckFile(bz2.BZ2File(di+"/"+fi), username, password)
+			fullFiNa = di+"/"+fi
+			stub, ext = os.path.splitext(fullFiNa)
+			if ext == ".bz2":
+				print fullFiNa
+				CheckFile(bz2.BZ2File(di+"/"+fi), username, password)
+			if ext == ".osm":
+				print fullFiNa
+				CheckFile(open(di+"/"+fi, "rt"), username, password)
 
 if __name__=="__main__":
-	fixTarget = str(55511190)
-	if len(sys.argv) >= 2:
-		fixTarget = sys.argv[1]
 
-	if 1:
-		username = raw_input("Username:") #"TimSCStreetViewImport"
+	parser = argparse.ArgumentParser(description='Fix broken ways.', add_help=False)
+	parser.add_argument('input', metavar='N', type=str, nargs='*',
+		               help='inputs to process')
+	parser.add_argument('--help', action='store_true', help='Print help message')
+	parser.add_argument('--way', action='store_true', help='Treat input as way ids (default)')
+	parser.add_argument('--file', action='store_true', help='Treat input as files')
+	parser.add_argument('--path', action='store_true', help='Treat input as paths')
+
+	parser.add_argument('--cred', help='Path to credentials file')
+
+	args = parser.parse_args()
+
+	if args.help or len(args.input) == 0:
+		parser.print_help()
+		exit(0)
+
+	if args.way and (args.file or args.path):
+		print "Only one input type may be specified"
+		exit(0)
+
+	if args.file and (args.way or args.path):
+		print "Only one input type may be specified"
+		exit(0)
+
+	if args.path and (args.way or args.file):
+		print "Only one input type may be specified"
+		exit(0)
+
+	if not args.path and not args.file:
+		args.way = True
+
+	if not args.cred:
+		username = raw_input("Username:")
 		password = raw_input("Password:")
 	else:
-		userFile = open("/home/tim/Desktop/user.txt","rt").read()
+		userFile = open(args.cred,"rt").read()
 		userData = userFile.split("\n")
 		username = userData[0]
 		password = userData[1]
 
-	#CheckAndFixWay(33266879, username, password)
-	
-	#WalkFiles("existing/12", username, password)
-	
-	if os.path.isfile(fixTarget):
-		CheckFile(open(fixTarget), username, password)
-	else:
-		wayId = int(fixTarget)
-		CheckAndFixWay(wayId, username, password)
+	for inp in args.input:
 
-
-
+		if args.file:
+			if not os.path.isfile(inp):
+				print "File not found:", inp
+				continue
+			CheckFile(open(inp, "rt"), username, password)
+		if args.way:
+			wayId = int(inp)
+			CheckAndFixWay(wayId, username, password)
+		if args.path:
+			WalkFiles(inp, username, password)
 
